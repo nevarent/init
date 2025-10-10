5298f345ee152ff1631bfcff30596106bb7a808b
nevarent <danielfuriel@gmail.com>
2025-10-10 20:07:18 +0300

    добавил config.h

---
+++ config.h
@@ -0,0 +1,3 @@
+static char *const rcinitcmd[]     = { "/bin/rc.init", NULL };
+static char *const rcrebootcmd[]   = { "/bin/rc.shutdown", "reboot", NULL };
+static char *const rcpoweroffcmd[] = { "/bin/rc.shutdown", "poweroff", NULL };
